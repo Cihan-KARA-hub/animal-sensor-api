7b3f5b77c74fe30f71e470c59415d17855af883d
Cihan Kara <cihankara632@gmail.com>
2025-05-09 16:01:06 +0300

    Email fix 🔧

--- cloud-server/src/main/java/com/yelman/cloudserver/CloudServerApplication.java
+++ cloud-server/src/main/java/com/yelman/cloudserver/CloudServerApplication.java
@@ -1,5 +1,6 @@
 package com.yelman.cloudserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CloudServerApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(CloudServerApplication.class, args);
     }
 
